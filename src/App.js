91785a08f9928217ab3683446267fb9c481e2cba
Mohammad Geranmayeh <m.geranmaye@gmail.com>
2022-06-18 09:49:39 +0430

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App bg-red-400 w-full">
+      <h1 className="text-red-400">Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
